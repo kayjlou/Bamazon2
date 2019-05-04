6e67554bc0ef782ac8939c8084162f1115d9e973
Katie Lewis <klewi314@gmail.com>
2019-05-04 10:47:14 -0700

    Added Select * query

---
+++ bamazon.js
@@ -0,0 +1,38 @@
+const { createConnection } = require("mysql2");
+const inquirer = require("inquirer");
+
+//Connection to database
+const db = createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Navigator13",
+  database: "bamazon2"
+});
+
+db.connect(e => {
+  if (e) {
+    console.log(e);
+  } else {
+    displayProducts();
+  }
+});
+
+const displayProducts = () => {
+  let query = "SELECT * FROM products";
+  db.query(query, (e, res) => {
+    if (e) {
+      console.log(e);
+    } else {
+      console.log(`ITEMS AVAILABLE FOR SALE: `);
+      for (let i = 0; i < res.length; i++) {
+        console.log(` 
+        Item id: ${res[i].item_id} || 
+        Product: ${res[i].product_name} || 
+        Department: ${res[i].department_name} ||
+        Price: ${res[i].price} ||
+        Quantity for sale: ${res[i].stock_quantity} ||
+        ----------------------------------------------`);
+      }
+    }
+  });
+};
